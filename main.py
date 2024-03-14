ab7ba7a82eb843832579c4fa7579da361ebbc43d
Danish Raza <danishrock11@gmail.com>
2024-03-15 00:16:50 +0500

    interpolate bbox and visualize

--- main.py
+++ main.py
@@ -20,10 +20,12 @@ vehicles = [2, 3, 5, 6, 7]
 # Read Frames
 frame_nmr = -1
 ret = True
+
 while ret:
     frame_nmr += 1
     ret, frame = cap.read()
-    if ret and frame_nmr < 10:
+    if ret:
+
         results[frame_nmr] = {}
         # Detect Vehicles
         detections = coco_model(frame)[0]
@@ -43,27 +45,29 @@ while ret:
 
             # Assign License Plate to Vehicle
             xcar1, ycar1, xcar2, ycar2, car_id = get_car(license_plate, track_ids)
+            print("Detected for Car ID: ", car_id)
 
-            # Crop license plate
-            license_plate_crop = frame[int(y1):int(y2), int(x1):int(x2), :]
+            if car_id != -1:
+                # Crop license plate
+                license_plate_crop = frame[int(y1):int(y2), int(x1):int(x2), :]
 
-            # Process License Plate
-            license_plate_crop_gray = cv2.cvtColor(license_plate_crop, cv2.COLOR_BGR2GRAY)
-            _, license_plate_crop_thresh = cv2.threshold(license_plate_crop_gray, 64, 255, cv2.THRESH_BINARY_INV)
+                # Process License Plate
+                license_plate_crop_gray = cv2.cvtColor(license_plate_crop, cv2.COLOR_BGR2GRAY)
+                _, license_plate_crop_thresh = cv2.threshold(license_plate_crop_gray, 64, 255, cv2.THRESH_BINARY_INV)
 
-            # Read License Plate Number
-            license_plate_text, license_plate_text_score = read_license_plate(license_plate_crop_thresh)
+                # Read License Plate Number
+                license_plate_text, license_plate_text_score = read_license_plate(license_plate_crop_thresh)
 
-            if license_plate_text is not None:
-                results[frame_nmr][car_id] = {'car': {'bbox': [xcar1, ycar1, xcar2, ycar2]},
-                                              'license_plate': {'bbox': [x1, y1, x2, y2],
-                                                                'bbox_score': score,
-                                                                'text': license_plate_text,
-                                                                'text_score': license_plate_text_score}
-                                              }
+                if license_plate_text is not None:
+                    results[frame_nmr][car_id] = {'car': {'bbox': [xcar1, ycar1, xcar2, ycar2]},
+                                                'license_plate': {'bbox': [x1, y1, x2, y2],
+                                                                    'bbox_score': score,
+                                                                    'text': license_plate_text,
+                                                                    'text_score': license_plate_text_score}
+                                                }
 
 # Write results
-write_csv(results, './results2.csv')
+write_csv(results, './results.csv')
 
 
 
--- util.py
+++ util.py
@@ -1,5 +1,6 @@
 import string
 import easyocr
+import re
 
 # Initialize the OCR reader
 reader = easyocr.Reader(['en'], gpu=False)
@@ -18,6 +19,54 @@ dict_int_to_char = {'0': 'O',
                     '4': 'A',
                     '6': 'G',
                     '5': 'S'}
+
+
+def license_complies_format(text):
+    """
+    Check if the license plate text complies with the required format.
+
+    Args:
+        text (str): License plate text.
+
+    Returns:
+        bool: True if the license plate complies with the format, False otherwise.
+    """
+    if len(text) != 7:
+        return False
+
+    if (text[0] in string.ascii_uppercase or text[0] in dict_int_to_char.keys()) and \
+       (text[1] in string.ascii_uppercase or text[1] in dict_int_to_char.keys()) and \
+       (text[2] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[2] in dict_char_to_int.keys()) and \
+       (text[3] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[3] in dict_char_to_int.keys()) and \
+       (text[4] in string.ascii_uppercase or text[4] in dict_int_to_char.keys()) and \
+       (text[5] in string.ascii_uppercase or text[5] in dict_int_to_char.keys()) and \
+       (text[6] in string.ascii_uppercase or text[6] in dict_int_to_char.keys()):
+        return True
+    else:
+        return False
+
+
+def format_license(text):
+    """
+    Format the license plate text by converting characters using the mapping dictionaries.
+
+    Args:
+        text (str): License plate text.
+
+    Returns:
+        str: Formatted license plate text.
+    """
+    license_plate_ = ''
+    mapping = {0: dict_int_to_char, 1: dict_int_to_char, 4: dict_int_to_char, 5: dict_int_to_char, 6: dict_int_to_char,
+               2: dict_char_to_int, 3: dict_char_to_int}
+    for j in [0, 1, 2, 3, 4, 5, 6]:
+        if text[j] in mapping[j].keys():
+            license_plate_ += mapping[j][text[j]]
+        else:
+            license_plate_ += text[j]
+
+    return license_plate_
+
 
 def write_csv(results, output_path):
     """
@@ -57,7 +106,17 @@ def write_csv(results, output_path):
         f.close()
 
 def read_license_plate(license_plate_crop):
-    return 0, 0
+    detections = reader.readtext(license_plate_crop)
+
+    for detection in detections: 
+        bbox, text, score = detection
+        text = text.upper().replace(' ', '')
+
+        if license_complies_format(text):
+            return format_license(text), score
+
+
+    return None, None
 
     
 
